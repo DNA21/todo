93f38586b35f0472dd805d76fbfe66670b26e797
Daniel Galuardi <dan.galuardi@gmail.com>
2023-04-17 16:03:50 -0400

    CSS styling

--- src/App.js
+++ src/App.js
@@ -4,28 +4,27 @@ import { Container } from 'reactstrap';
 import { TiPlus } from 'react-icons/ti';
 import Todo from './Todo';
 
-const style = {
-
-}
 
 function App() {
     const [todos, setTodos] = useState(['Learn React', 'Grind Leetcode']);
 
     return (
         <>
-            <div className='page-background'>
-                <Container className='bg-light'>
-                    <h3>ToDo App</h3>
-                    <form className={style.form}>
-                        <input className={style.input} type="text" placeholder='Add Todo' />
-                        <button className={style.button}><TiPlus /></button>
+            <div className='page-background pt-5'>
+                <Container className='bg-light rounded-3 p-3 shadow'>
+                    <h3 className='font-bold text-center'>ToDo App</h3>
+                    <form className='d-flex justify-content-between'>
+                        <div className="input-group input-group-lg">
+                            <input className='border form-control' type="text" placeholder='Add Todo' />
+                            <button className='btn btn-outline-secondary'><TiPlus size={30}/></button>
+                        </div>
                     </form>
-                    <ul>
+                    <ul className='p-0'>
                         {todos.map((todo, index) => (
                             <Todo key={index} todo={todo} />
                         ))}
                     </ul>
-                    <p>You have 2 todos</p>
+                    <p className='text-center p-2'>You have 2 todos</p>
                 </Container>
             </div>
         </>
--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BiTrash } from 'react-icons/bi';
+import { Input, Row, Col, Label, FormGroup } from 'reactstrap';
 
 const Todo = ({todo}) => {
     return (
-        <li>
-            <div>
-                <input type='checkbox' />
-                <p className=''>{todo}</p>
-            </div>
-            <button><BiTrash /></button>
+        <li className='d-flex justify-content-between text-capitalize list-background rounded-3 px-4 py-2 my-3'>
+            <Row>
+                <Col className='my-auto'>
+                    <Input className='form-check py-auto pointer' type='checkbox' />
+                </Col>
+                <Col xs='auto'>
+                    <p className='mb-0 fs-3'>{todo}</p>
+                </Col>
+            </Row>
+            <button><BiTrash size={25} /></button>
         </li>
     )
 }
