d0a1d52ffc2fedeb6e76dac18a11f3eb1b954722
Daniel Galuardi <dan.galuardi@gmail.com>
2023-04-17 17:21:04 -0400

    CRUD opperations

--- src/App.js
+++ src/App.js
@@ -1,30 +1,78 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
 import { Container } from 'reactstrap';
 import { TiPlus } from 'react-icons/ti';
 import Todo from './Todo';
+import {db} from './firebase';
+import {query, collection, onSnapshot, updateDoc, doc, addDoc, deleteDoc} from 'firebase/firestore';
 
 
 function App() {
-    const [todos, setTodos] = useState(['Learn React', 'Grind Leetcode']);
+    const [todos, setTodos] = useState([]);
+    const [input, setInput] = useState('');
+
+    // Create todo
+    const createTodo = async (e) => {
+        e.preventDefault(e)
+        if(input === '') {
+            alert('Please enter a todo')
+            return
+        }
+        await addDoc(collection(db, 'todos'), {
+            text: input,
+            completed: false
+        })
+        setInput('');
+    }
+
+    //Read todo from Firebase
+    useEffect(() => {
+        const q =query(collection(db, 'todos'));
+        const unscubscribe = onSnapshot(q, (querySnapshot) => {
+            let todosArray = [];
+            querySnapshot.forEach((doc) => {
+                todosArray.push({...doc.data(), id: doc.id})
+            });
+            setTodos(todosArray);
+        })
+        return () => unscubscribe();
+    }, [])
+
+    //Update todo in firebase
+    const toggleComplete = async (todo) => {
+        await updateDoc(doc(db, 'todos', todo.id), {
+            completed: !todo.completed
+        })
+    }
+
+    //Delete todo
+    const deleteTodo = async (id) => {
+        await deleteDoc(doc(db, 'todos', id))
+    }
 
     return (
         <>
             <div className='page-background pt-5'>
                 <Container className='bg-light rounded-3 p-3 shadow'>
                     <h3 className='font-bold text-center'>ToDo App</h3>
-                    <form className='d-flex justify-content-between'>
+                    <form onSubmit={createTodo} className='d-flex justify-content-between'>
                         <div className="input-group input-group-lg">
-                            <input className='border form-control' type="text" placeholder='Add Todo' />
-                            <button className='btn btn-outline-secondary'><TiPlus size={30}/></button>
+                            <input
+                                value={input}
+                                onChange={(e)=> setInput(e.target.value)}
+                                className='border form-control'
+                                type="text"
+                                placeholder='Add Todo'
+                            />
+                            <button className='btn btn-outline-secondary btn-success'><TiPlus size={30} color='white' /></button>
                         </div>
                     </form>
                     <ul className='p-0'>
                         {todos.map((todo, index) => (
-                            <Todo key={index} todo={todo} />
+                            <Todo key={index} todo={todo} toggleComplete={toggleComplete} deleteTodo={deleteTodo} />
                         ))}
                     </ul>
-                    <p className='text-center p-2'>You have 2 todos</p>
+                    <p className='text-center p-2'>{`You have ${todos.length} todos`}</p>
                 </Container>
             </div>
         </>
--- src/Todo.js
+++ src/Todo.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { BiTrash } from 'react-icons/bi';
 import { Input, Row, Col, Label, FormGroup } from 'reactstrap';
 
-const Todo = ({todo}) => {
+const Todo = ({ todo, toggleComplete, deleteTodo }) => {
     return (
-        <li className='d-flex justify-content-between text-capitalize list-background rounded-3 px-4 py-2 my-3'>
+        <li className={`d-flex justify-content-between text-capitalize rounded-3 px-4 py-2 my-3 ${todo.completed ? 'list-background-completed' : 'list-background'}`}>
             <Row>
                 <Col className='my-auto'>
-                    <Input className='form-check py-auto pointer' type='checkbox' />
+                    <Input onChange={() => toggleComplete(todo)} className='form-check py-auto pointer' type='checkbox' checked={todo.completed ? 'checked' : ''} />
                 </Col>
                 <Col xs='auto'>
-                    <p className='mb-0 fs-3'>{todo}</p>
+                    <p className='mb-0 fs-3' onClick={() => toggleComplete(todo)}>
+                        {todo.completed
+                        ? (<s>{todo.text}</s>)
+                        : (<>{todo.text}</>)}
+                    </p>
                 </Col>
             </Row>
-            <button><BiTrash size={25} /></button>
+            <button onClick={() => deleteTodo(todo.id)}><BiTrash size={25} /></button>
         </li>
     )
 }
