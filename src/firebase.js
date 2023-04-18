04ff55f1855d847bcb64956d4e837246329f896a
Daniel Galuardi <dan.galuardi@gmail.com>
2023-04-18 13:25:35 -0400

    Firebase deployment

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "./firebaseConfig";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDjPPvKB3oSh_nnD-x8VsSBqF2pGbLXsws",
-  authDomain: "todo-app-46808.firebaseapp.com",
-  projectId: "todo-app-46808",
-  storageBucket: "todo-app-46808.appspot.com",
-  messagingSenderId: "1018603707928",
-  appId: "1:1018603707928:web:4da77850a3f18768392d6d"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDjPPvKB3oSh_nnD-x8VsSBqF2pGbLXsws",
+//   authDomain: "todo-app-46808.firebaseapp.com",
+//   projectId: "todo-app-46808",
+//   storageBucket: "todo-app-46808.appspot.com",
+//   messagingSenderId: "1018603707928",
+//   appId: "1:1018603707928:web:4da77850a3f18768392d6d"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDjPPvKB3oSh_nnD-x8VsSBqF2pGbLXsws",
+    authDomain: "todo-app-46808.firebaseapp.com",
+    projectId: "todo-app-46808",
+    storageBucket: "todo-app-46808.appspot.com",
+    messagingSenderId: "1018603707928",
+    appId: "1:1018603707928:web:4da77850a3f18768392d6d"
+  };
